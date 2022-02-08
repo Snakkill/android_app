3c0966bfc9743ad1be2f17fad014d61ec29f0081
Ayman <aymanmoubayed@gmail.com>
2022-02-08 09:34:33 -0600

    onlick toast

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.firstapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText nameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nameText=findViewById(R.id.etName);
+    }
+
+    public void toastName(View view) {
+        Toast.makeText(this,nameText.getText().toString(),Toast.LENGTH_SHORT).show();
     }
 }
