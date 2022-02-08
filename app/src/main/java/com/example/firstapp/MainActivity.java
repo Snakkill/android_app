ad203cce143c8968ec2babea590d63b05f192340
Ayman <aymanmoubayed@gmail.com>
2022-02-08 11:53:52 -0600

    focus

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -7,16 +7,31 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnFocusChangeListener {
     EditText nameText;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        nameText=findViewById(R.id.etName);
+
+        nameText = findViewById(R.id.etName);
+        nameText.setOnFocusChangeListener(this);
+
     }
 
     public void toastName(View view) {
         Toast.makeText(this,nameText.getText().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void onFocusChange(View view,boolean focus) {
+
+        if(focus){
+            Toast.makeText(this,"focus",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this,"lost focus",Toast.LENGTH_SHORT).show();
+        }
     }
 }
