d20819e638b1976e385c0319ddd9e9793b783f09
Ayman <aymanmoubayed@gmail.com>
2022-02-15 17:52:01 -0600

    SMS pulled and logged

--- app/src/main/java/com/example/firstapp/SMS.java
+++ app/src/main/java/com/example/firstapp/SMS.java
@@ -1,8 +1,13 @@
 package com.example.firstapp;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsMessage;
+import android.util.Log;
 import android.widget.Toast;
 
 public class SMS extends BroadcastReceiver {
@@ -12,5 +17,31 @@ public class SMS extends BroadcastReceiver {
         // TODO: This method is called when the BroadcastReceiver is receiving
         // an Intent broadcast.
         Toast.makeText(context,"THE APP IS MAKING A TOAST",Toast.LENGTH_SHORT).show();
+
+        if(intent.getAction().equals("android.provider.Telephony.SMS_RECEIVED")){
+            Bundle bundle = intent.getExtras();           //---get the SMS message passed in---
+            SmsMessage[] msgs = null;
+            String msg_from;
+            if (bundle != null){
+                //---retrieve the SMS message received---
+                try{
+                    Object[] pdus = (Object[]) bundle.get("pdus");
+                    msgs = new SmsMessage[pdus.length];
+                    for(int i=0; i<msgs.length; i++){
+                        msgs[i] = SmsMessage.createFromPdu((byte[])pdus[i]);
+                        msg_from = msgs[i].getOriginatingAddress();
+                        String msgBody = msgs[i].getMessageBody();
+
+
+
+                        Log.d(TAG, msgBody);
+                    }
+                }catch(Exception e){
+//                            Log.d("Exception caught",e.getMessage());
+                }
+            }
+        }
     }
 }
+
+
